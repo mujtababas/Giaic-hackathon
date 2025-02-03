5ddd4748dc16ba027cafab82e87b5d2a55ef0e42
Syed Mujtaba Abbas <abbasmujtaba125@gmail.com>
2025-02-03 11:10:53 +0500

    Added all project files

---
+++ day-7-complete/NikeJordee/pages/success.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import './success.css';
+
+const SuccessStripe = () => {
+  return (
+    <div className="success-page">
+      <div className="success-container">
+        <h1>Order Successfully Placed!</h1>
+        <div className="success-details">
+          <h2>Thank You,</h2>
+          <p>Your order has been successfully placed and is now being processed.</p>
+        </div>
+        <Link href={"/"}>
+         Back to Homepage
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessStripe;
